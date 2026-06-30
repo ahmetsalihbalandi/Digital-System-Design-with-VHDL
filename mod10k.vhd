ce4cc23482a0d7247abf4ab047be91fdada3e4ab
Ahmet Salih Balandi <salihbalandi@gmail.com>
2026-06-30 14:21:48 +0000

    Autonomous Update: mod10k.vhd (Part 1)

---
+++ mod10k.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mod10k is
+    Port ( clr : in STD_LOGIC;
+           clk : in STD_LOGIC;
+           q : out STD_LOGIC_VECTOR (13 downto 0));
+end mod10k;
+
+architecture Behavioral of mod10k is
+
+begin
+
+
+end Behavioral;
